eeb154fcadc8ca8cf48f9d80dfb01322f2bda75a
Yashvi Ghetiya <patelyasvi495@gmail.com>
2026-05-14 15:05:56 +0530

    updated

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,20 +1,20 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "../lib/utils"
+import { Button } from "../components/ui/button"
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "../components/ui/card"
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
   FieldSeparator,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
+} from "../components/ui/field"
+import { Input } from "../components/ui/input"
 
 export function LoginForm({
   className,
